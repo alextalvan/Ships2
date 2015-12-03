844b7b2b7a65426a70746ca72c0a55c2b1e02d2d
Ohmu93 <dekan51@gmail.com>
2015-12-03 13:14:13 +0100

    Whale and pickups random spawns implemented

--- Assets/Scripts/SpawnDebris.cs
+++ Assets/Scripts/SpawnDebris.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine.Networking;
 
-public class SpawnDebris : MonoBehaviour {
+public class SpawnDebris : NetworkBehaviour {
+
+    [SerializeField]
+    private List<GameObject> debris = new List<GameObject>();
 
     List<GameObject> debrisList = new List<GameObject>();
 
     [SerializeField]
     Transform sea;
-
-    [SerializeField]
-    GameObject pickupPrefab;
-
-    GameObject debris;
 
     float existanceTime = 0;
 
@@ -23,18 +21,21 @@ public class SpawnDebris : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-        existanceTime += Time.fixedDeltaTime;
-        if (existanceTime >= 30)
+	void Update ()
+    {
+        existanceTime += Time.deltaTime;
+        if (existanceTime >= 30f)
         {
             debrisList.Clear();
-            existanceTime = 0;
+            existanceTime = 0f;
         }
-        if (debrisList.Count <= 10)
+        if (debrisList.Count <= 10f)
         {
             spawnDebris();
         }
     }
+
+    [ServerCallback]
     void spawnDebris() {
         Vector3 rndPosWithinSea;
         rndPosWithinSea = new Vector3(Random.Range(-450f, 350f), Random.Range(-1, 1f), Random.Range(0f, 800f));
@@ -48,10 +49,11 @@ public class SpawnDebris : MonoBehaviour {
                
             }
         }
-        debris = (GameObject)Instantiate(pickupPrefab, rndPosWithinSea, Quaternion.identity);
-        NetworkServer.Spawn(debris);
-        debrisList.Add(debris);
-        
 
+        int rndDebr = Random.Range(0, debris.Count);
+
+        GameObject rndDebris = (GameObject)Instantiate(debris[rndDebr], rndPosWithinSea, Quaternion.identity);
+        NetworkServer.Spawn(rndDebris);
+        debrisList.Add(rndDebris);
     }
 }
